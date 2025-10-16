be83bfdbc69bb298bb5253c5e8451851d796d596
Biglands <151714415+BigLands1@users.noreply.github.com>
2025-10-15 21:25:23 -0400

    Delete scripts directory

--- scripts/remove_bom.ps1
+++
@@ -1,7 +0,0 @@
-Param([string]$Path)
-$bytes = [System.IO.File]::ReadAllBytes($Path)
-if ($bytes.Length -ge 3 -and $bytes[0] -eq 0xEF -and $bytes[1] -eq 0xBB -and $bytes[2] -eq 0xBF) {
-    $new = New-Object byte[] ($bytes.Length - 3)
-    [System.Array]::Copy($bytes, 3, $new, 0, $new.Length)
-    [System.IO.File]::WriteAllBytes($Path, $new)
-}
